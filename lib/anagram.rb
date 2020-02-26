58b66e9d40776b48a54218086966213fb3826c5b
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-26 23:32:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+ 
+ attr_reader :anagram 
+ 
+ ANAGRAM = %w[cars potatoes racs four scar creams scream] 
+  
+  def initialize(word)
+    
+    @anagram = word
+  end
+ 
+  
+  def match(anagram)
+    
+    anagram.group_by{|w| w.downcase.each_char.sort}.values.each{|v| v.uniq!}
+  end
+end
+
+    
+    
